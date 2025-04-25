ed9cf948aa55e79b2e5f8c9b6e33fefbc92e86bb
LazyTurtle12 <93883571+LazyTurtle12@users.noreply.github.com>
2025-04-25 19:13:11 +0300

    Add files via upload

--- stay_alive.py
+++ stay_alive.py
@@ -1,24 +1,16 @@
 
-
-from flask import Flask, render_template
+from flask import Flask
 from threading import Thread
-import os
-from stay_alive import keep_alive
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-  replit_url = f'https://{os.environ["REPL_SLUG"]}.{os.environ["REPL_OWNER"]}.replit.co'
-  return f"Alive! Replit Project URL: {replit_url}"
+    return "Bot is Alive! (Hosted on Render)"
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
-
-
-keep_alive()
+    t = Thread(target=run)
+    t.start()
